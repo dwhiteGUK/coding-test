4137063d49bf16ed028c055863488f2af5516bdf
Darren White <dwhite@gentingcasinos.co.uk>
2021-07-25 11:32:07 +0100

    feat: :sparkles: Fetch album data

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,43 +13,21 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Albums
         </h1>
+        {!data && <p><strong>No Albums Found</strong></p>}
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        {data && (
+          <ul>
+            {data.map(({ id, title }) => (
+              <>
+                <li key={id}>{title}</li>
+              </>
+            ))}
+          </ul>
+        )}
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer className={styles.footer}>
@@ -64,6 +42,22 @@ export default function Home() {
           </span>
         </a>
       </footer>
-    </div>
+    </div >
   )
+}
+
+export async function getStaticProps() {
+
+  const res = await fetch('https://jsonplaceholder.typicode.com/albums')
+  const data = await res.json()
+
+  if (res.status === 404 && res.statusText === 'Not Found') {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { data },
+  }
 }
